a230ac94bf4397c1f35de2a0f48960f3074d2ec3
Herb <ghstahl@gmail.com>
2016-06-23 15:22:29 -0700

    Added modifed user unittests

--- src/AspNet.Identity.Cassandra.Tests/Store/UserStoreTest.cs
+++ src/AspNet.Identity.Cassandra.Tests/Store/UserStoreTest.cs
@@ -14,6 +14,325 @@ namespace AspNet.Identity.Cassandra.Tests
     public class UserStoreTest
     {
         [TestMethod]
+        public async Task Test_add_user_modify_email_confirmed_delete()
+        {
+            var dao = Global.TenantDao;
+            await dao.EstablishConnectionAsync();
+
+            var guidId = Guid.NewGuid();
+            var userStore = new CassandraUserStore();
+
+            string userName = Guid.NewGuid().ToString();
+            var user = new CassandraUser()
+            {
+                Email = userName,
+                UserName = userName,
+                EmailConfirmed = false
+            };
+            await userStore.CreateAsync(user);
+
+            var foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNotNull(foundUser);
+            Assert.IsFalse(await userStore.GetEmailConfirmedAsync(foundUser));
+
+            await userStore.SetEmailConfirmedAsync(foundUser, true);
+            await userStore.UpdateAsync(foundUser);
+
+            foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNotNull(foundUser);
+            Assert.IsTrue(await userStore.GetEmailConfirmedAsync(foundUser));
+
+            await userStore.DeleteAsync(foundUser);
+            foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNull(foundUser);
+            
+
+
+        }
+
+        [TestMethod]
+        public async Task Test_add_user_modify_phone_confirmed_delete()
+        {
+            var dao = Global.TenantDao;
+            await dao.EstablishConnectionAsync();
+
+            var guidId = Guid.NewGuid();
+            var userStore = new CassandraUserStore();
+
+            string userName = Guid.NewGuid().ToString();
+            var user = new CassandraUser()
+            {
+                Email = userName,
+                UserName = userName,
+                EmailConfirmed = false,
+                PhoneNumberConfirmed = false
+            };
+            await userStore.CreateAsync(user);
+
+            var foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNotNull(foundUser);
+            Assert.IsFalse(await userStore.GetPhoneNumberConfirmedAsync(foundUser));
+
+            await userStore.SetPhoneNumberConfirmedAsync(foundUser, true);
+            await userStore.UpdateAsync(foundUser);
+
+            foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNotNull(foundUser);
+            Assert.IsTrue(await userStore.GetPhoneNumberConfirmedAsync(foundUser));
+
+            await userStore.DeleteAsync(foundUser);
+            foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNull(foundUser);
+
+
+
+        }
+
+        [TestMethod]
+        public async Task Test_add_user_modify_phone_number_delete()
+        {
+            var dao = Global.TenantDao;
+            await dao.EstablishConnectionAsync();
+
+            var guidId = Guid.NewGuid();
+            var userStore = new CassandraUserStore();
+
+            string userName = Guid.NewGuid().ToString();
+            var user = new CassandraUser()
+            {
+                Email = userName,
+                UserName = userName,
+                EmailConfirmed = false,
+                PhoneNumberConfirmed = false,
+                PhoneNumber = "310.383.1111"
+            };
+            await userStore.CreateAsync(user);
+
+            var foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNotNull(foundUser);
+            Assert.AreEqual(await userStore.GetPhoneNumberAsync(foundUser), "310.383.1111");
+
+            await userStore.SetPhoneNumberAsync(foundUser, "310.383.2222");
+            await userStore.UpdateAsync(foundUser);
+
+            foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNotNull(foundUser);
+            Assert.AreEqual(await userStore.GetPhoneNumberAsync(foundUser), "310.383.2222");
+
+            await userStore.DeleteAsync(foundUser);
+            foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNull(foundUser);
+
+
+
+        }
+
+        [TestMethod]
+        public async Task Test_add_user_modify_lockout_enabled_delete()
+        {
+            var dao = Global.TenantDao;
+            await dao.EstablishConnectionAsync();
+
+            var guidId = Guid.NewGuid();
+            var userStore = new CassandraUserStore();
+
+            string userName = Guid.NewGuid().ToString();
+            var user = new CassandraUser()
+            {
+                Email = userName,
+                UserName = userName,
+                EmailConfirmed = false,
+                PhoneNumberConfirmed = false,
+                PhoneNumber = "310.383.1111",
+                LockoutEnabled = false
+            };
+            await userStore.CreateAsync(user);
+
+            var foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNotNull(foundUser);
+            Assert.IsFalse(await userStore.GetLockoutEnabledAsync(foundUser));
+
+            await userStore.SetLockoutEnabledAsync(foundUser, true);
+            await userStore.UpdateAsync(foundUser);
+
+            foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNotNull(foundUser);
+            Assert.IsTrue(await userStore.GetLockoutEnabledAsync(foundUser));
+
+            await userStore.DeleteAsync(foundUser);
+            foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNull(foundUser);
+
+
+
+        }
+        [TestMethod]
+        public async Task Test_add_user_modify_password_hash_delete()
+        {
+            var dao = Global.TenantDao;
+            await dao.EstablishConnectionAsync();
+
+            var guidId = Guid.NewGuid();
+            var userStore = new CassandraUserStore();
+
+            string userName = Guid.NewGuid().ToString();
+            var user = new CassandraUser()
+            {
+                Email = userName,
+                UserName = userName,
+                EmailConfirmed = false,
+                PhoneNumberConfirmed = false,
+                PhoneNumber = "310.383.1111",
+                LockoutEnabled = false,
+                PasswordHash = "1234"
+            };
+            await userStore.CreateAsync(user);
+
+            var foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNotNull(foundUser);
+            Assert.AreEqual(await userStore.GetPasswordHashAsync(foundUser),"1234");
+
+            await userStore.SetPasswordHashAsync(foundUser, "abcd");
+            await userStore.UpdateAsync(foundUser);
+
+            foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNotNull(foundUser);
+            Assert.AreEqual(await userStore.GetPasswordHashAsync(foundUser), "abcd");
+
+            await userStore.DeleteAsync(foundUser);
+            foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNull(foundUser);
+
+
+
+        }
+        [TestMethod]
+        public async Task Test_add_user_modify_security_stamp_delete()
+        {
+            var dao = Global.TenantDao;
+            await dao.EstablishConnectionAsync();
+
+            var guidId = Guid.NewGuid();
+            var userStore = new CassandraUserStore();
+
+            string userName = Guid.NewGuid().ToString();
+            var user = new CassandraUser()
+            {
+                Email = userName,
+                UserName = userName,
+                EmailConfirmed = false,
+                PhoneNumberConfirmed = false,
+                PhoneNumber = "310.383.1111",
+                LockoutEnabled = false,
+                PasswordHash = "1234",
+                SecurityStamp = "1234"
+            };
+            await userStore.CreateAsync(user);
+
+            var foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNotNull(foundUser);
+            Assert.AreEqual(await userStore.GetSecurityStampAsync(foundUser), "1234");
+
+            await userStore.SetSecurityStampAsync(foundUser, "abcd");
+            await userStore.UpdateAsync(foundUser);
+
+            foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNotNull(foundUser);
+            Assert.AreEqual(await userStore.GetSecurityStampAsync(foundUser), "abcd");
+
+            await userStore.DeleteAsync(foundUser);
+            foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNull(foundUser);
+        }
+        [TestMethod]
+        public async Task Test_add_user_modify_lockout_end_date_delete()
+        {
+            var dao = Global.TenantDao;
+            await dao.EstablishConnectionAsync();
+
+            var guidId = Guid.NewGuid();
+            var userStore = new CassandraUserStore();
+
+            string userName = Guid.NewGuid().ToString();
+            var user = new CassandraUser()
+            {
+                Email = userName,
+                UserName = userName,
+                EmailConfirmed = false,
+                PhoneNumberConfirmed = false,
+                PhoneNumber = "310.383.1111",
+                LockoutEnabled = false,
+                PasswordHash = "1234",
+                SecurityStamp = "1234",
+                LockoutEndDate = DateTimeOffset.UtcNow
+            };
+            await userStore.CreateAsync(user);
+
+            var foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNotNull(foundUser);
+            var lockOutDate = await userStore.GetLockoutEndDateAsync(foundUser);
+            Assert.AreEqual(lockOutDate.Day, user.LockoutEndDate.Day);
+            Assert.AreEqual(lockOutDate.Hour, user.LockoutEndDate.Hour);
+            Assert.AreEqual(lockOutDate.Minute, user.LockoutEndDate.Minute);
+            Assert.AreEqual(lockOutDate.Year, user.LockoutEndDate.Year);
+
+            var future = user.LockoutEndDate.AddDays(5);
+            await userStore.SetLockoutEndDateAsync(foundUser, future);
+            await userStore.UpdateAsync(foundUser);
+
+            foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNotNull(foundUser);
+            lockOutDate = await userStore.GetLockoutEndDateAsync(foundUser);
+            Assert.AreEqual(lockOutDate.Day, future.Day);
+            Assert.AreEqual(lockOutDate.Hour, future.Hour);
+            Assert.AreEqual(lockOutDate.Minute, future.Minute);
+            Assert.AreEqual(lockOutDate.Year, future.Year);
+
+            await userStore.DeleteAsync(foundUser);
+            foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNull(foundUser);
+            
+        }
+        [TestMethod]
+        public async Task Test_add_user_modify_twofactor_enabled_delete()
+        {
+            var dao = Global.TenantDao;
+            await dao.EstablishConnectionAsync();
+
+            var guidId = Guid.NewGuid();
+            var userStore = new CassandraUserStore();
+
+            string userName = Guid.NewGuid().ToString();
+            var user = new CassandraUser()
+            {
+                Email = userName,
+                UserName = userName,
+                EmailConfirmed = false,
+                PhoneNumberConfirmed = false,
+                PhoneNumber = "310.383.1111",
+                LockoutEnabled = false,
+                PasswordHash = "1234",
+                SecurityStamp = "1234",
+                TwoFactorEnabled = false
+            };
+            await userStore.CreateAsync(user);
+
+            var foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNotNull(foundUser);
+            Assert.IsFalse(await userStore.GetTwoFactorEnabledAsync(foundUser));
+
+            await userStore.SetTwoFactorEnabledAsync(foundUser, true);
+            await userStore.UpdateAsync(foundUser);
+
+            foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNotNull(foundUser);
+            Assert.IsTrue(await userStore.GetTwoFactorEnabledAsync(foundUser));
+
+            await userStore.DeleteAsync(foundUser);
+            foundUser = await userStore.FindByEmailAsync(userName);
+            Assert.IsNull(foundUser);
+        }
+        [TestMethod]
         public async Task Test_Add_Full_User_Find_by_all_Delete_User_Async()
         {
             var dao = Global.TenantDao;
