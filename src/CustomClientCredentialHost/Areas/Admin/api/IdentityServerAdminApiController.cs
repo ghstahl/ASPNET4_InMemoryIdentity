6b7048d1292483f5116e3443feac427e7e693fff
Herb <ghstahl@gmail.com>
2016-08-02 11:04:48 -0700

    Added some Scopes REST Apis get/delete/post

--- src/CustomClientCredentialHost/Areas/Admin/api/IdentityServerAdminApiController.cs
+++ src/CustomClientCredentialHost/Areas/Admin/api/IdentityServerAdminApiController.cs
@@ -10,36 +10,9 @@ using P5.IdentityServer3.Cassandra;
 namespace CustomClientCredentialHost.Areas.Admin.api
 {
     [RoutePrefix("api/v1/IDSAdmin")]
-    public class IdentityServerAdminApiController : ApiController
+    public partial class IdentityServerAdminApiController : ApiController
     {
-        // GET api/<controller>
-        [Route("scopes/page/{pageSize:int}")]
-        [HttpGet]
-        public async Task<IDSScopePageRecord> PageScopsesAsync(int pageSize, string pagingState)
-        {
-            var adminStore = new IdentityServer3AdminStore();
-
-
-            var page = await adminStore.PageScopesAsync(pageSize, null);
-            var state = HttpUtility.UrlEncode(page.PagingState);
-            var record = new IDSScopePageRecord()
-            {
-                CurrentPagingState = HttpUtility.UrlEncode(page.CurrentPagingState),
-                PageSize = pageSize,
-                PagingState = HttpUtility.UrlEncode(page.PagingState),
-                Scopes = page
-            };
-            return record;
-        }
-
-        [Route("scopes/find")]
-        [HttpGet]
-        public async Task<Scope> FindScopeAsync(string name)
-        {
-            var adminStore = new IdentityServer3AdminStore();
-            var scope = await adminStore.FindScopeByNameAsync(name);
-            return scope;
-        }
+       
         [Authorize]
         [Route("who")]
         [HttpGet]
---
+++ src/CustomClientCredentialHost/Areas/Admin/api/Scopes/IdentityServerAdminApiController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using CustomClientCredentialHost.Areas.Admin.Models;
+using IdentityServer3.Core.Models;
+using P5.IdentityServer3.Cassandra;
+
+namespace CustomClientCredentialHost.Areas.Admin.api
+{
+    public partial class IdentityServerAdminApiController : ApiController
+    {
+        // GET api/<controller>
+        [Route("scopes/page/{pageSize:int}")]
+        [HttpGet]
+        public async Task<IDSScopePageRecord> PageScopsesAsync(int pageSize, string pagingState)
+        {
+            var adminStore = new IdentityServer3AdminStore();
+
+
+            var page = await adminStore.PageScopesAsync(pageSize, null);
+            var state = HttpUtility.UrlEncode(page.PagingState);
+            var record = new IDSScopePageRecord()
+            {
+                CurrentPagingState = HttpUtility.UrlEncode(page.CurrentPagingState),
+                PageSize = pageSize,
+                PagingState = HttpUtility.UrlEncode(page.PagingState),
+                Scopes = page
+            };
+            return record;
+        }
+
+        [Route("scopes")]
+        [HttpGet]
+        public async Task<Scope> FindScopeAsync(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("name");
+            }
+            var adminStore = new IdentityServer3AdminStore();
+            var scope = await adminStore.FindScopeByNameAsync(name);
+            return scope;
+        }
+
+        [Route("scopes")]
+        [HttpDelete]
+        public async Task DeleteScopeAsync(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("name");
+            }
+            var adminStore = new IdentityServer3AdminStore();
+            await adminStore.DeleteScopeAsync(new Scope(){Name = name});
+        }
+
+        [Route("scopes")]
+        [HttpPost]
+        public async Task UpsertScopeAsync(Scope scope)
+        {
+            if (scope == null)
+            {
+                throw new ArgumentNullException("scope");
+            }
+            var adminStore = new IdentityServer3AdminStore();
+            await adminStore.CreateScopeAsync(scope);
+        }
+    }
+}
